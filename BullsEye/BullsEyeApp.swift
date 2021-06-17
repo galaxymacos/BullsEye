a9ed6910dcbdcd8a37577d8eb0b16d506837b6a4
Xun Ruan <galaxymacos@gmail.com>
2021-06-17 09:57:07 +0800

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Xun Ruan on 2021/6/17.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
